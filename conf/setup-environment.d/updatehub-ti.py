5af61609f8a8f0798cbe5aea34a4c498c5b74bf3
Fabio Berton <fabio.berton@ossystems.com.br>
2017-08-21 17:01:01 -0300

    Initial Layer Structure
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

---
+++ conf/setup-environment.d/updatehub-ti.py
@@ -0,0 +1,10 @@
+def __after_init_updatehub_ti():
+    PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+
+    append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+                    [
+                        'meta-ti',
+                        'meta-updatehub-ti',
+                    ]])
+
+run_after_init(__after_init_updatehub_ti)
